7465c497c47065f03b60a5e59d45b4c603d0d341
mee1b <mee1b@yandex.ru>
2024-10-16 21:25:18 +0900

    Initial commit

---
+++ directive/directive.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+static void add(int one, int two)
+{
+    std::cout << "Результат сложения: " << one + two << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int one{}, two{};
+
+#ifndef MODE
+    std::cout << "Определите MODE!";
+    return EXIT_FAILURE;
+#endif
+
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки";
+    return EXIT_SUCCESS;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме\n";
+    std::cout << "Введите число 1: ";
+    std::cin >> one;
+    std::cout << "Введите число 2: ";
+    std::cin >> two;
+    add(one, two);
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+}
+
